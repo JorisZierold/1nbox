26fbc726355ce539dc3c3d99d792d030dd0c1fff
Joris | zierold.eth <joris@zierold.ventures>
2025-07-29 22:07:33 +0200

    feat: add chain logos

---
+++ apps/web/lib/chains.ts
@@ -0,0 +1,19 @@
+export const getChainIcon = (chainName: string): string => {
+  const iconMap: Record<string, string> = {
+    Ethereum: "/chains/ethereum.svg",
+    "Arbitrum One": "/chains/arbitrum.svg",
+    Polygon: "/chains/polygon.svg",
+    Base: "/chains/base.svg",
+    Optimism: "/chains/optimism.svg",
+    Avalanche: "/chains/avalanche.svg",
+    "zkSync Era": "/chains/zksync.svg",
+    Solana: "/chains/solana.svg",
+    "BNB Smart Chain": "/chains/bnb.svg",
+    Gnosis: "/chains/gnosis.svg",
+    Linea: "/chains/linea.svg",
+    Sonic: "/chains/sonic.svg",
+    UniChain: "/chains/unichain.svg",
+  };
+
+  return iconMap[chainName] || "/chains/ethereum.svg";
+};
